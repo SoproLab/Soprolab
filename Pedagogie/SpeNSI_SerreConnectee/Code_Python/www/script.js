32c89c6e048a880dc15f45e6b36c9e93d5d0c37e
SoproLab <jacques@j-chouteau.org>
2020-10-22 00:10:09 +0200

    mini_serre_02

--- Pedagogie/SpeNSI_SerreConnectee/Code_Python/www/script.js
+++ Pedagogie/SpeNSI_SerreConnectee/Code_Python/www/script.js
@@ -1,5 +1,6 @@
 // Solution proposée : création d'un fichier javascript associé au HTML
-// Le code Python de la fonction page_Web ( ...) est chargé de réaliser la page HTML envoyée au navigateur. C'est dans cette fonction que l'on remplacera <variable_toit> par l'état de la trappe
+// Le code Python de la fonction page_Web ( ...) est chargé de réaliser la page HTML envoyée au navigateur.
+// C'est dans cette fonction que l'on remplacera <variable_toit> par l'état de la trappe.
 let toit = "<variable_toit>"
 let bp_open = document.getElementById("bp_open")
 let bp_close = document.getElementById("bp_close")
@@ -10,5 +11,5 @@ if (toit == "close" ) {
 }
 // Si le toit est ouvert 
 if (toit == "open" ) {
-bp_open.className += 'hidden'; // Cacher FERMER   
+bp_open.className += 'hidden'; // Cacher OUVRIR   
 }
