2799081e31dd39c9d27cb09adecda0ace3e5e720
SoproLab <jacques@j-chouteau.org>
2021-01-12 13:25:53 +0100

    Update essai_02.py

--- Pedagogie/SyntheseAdditive_NeoPixel/essai_02.py
+++ Pedagogie/SyntheseAdditive_NeoPixel/essai_02.py
@@ -8,20 +8,20 @@ from time import sleep
 led = NeoPixel ( Pin(2), 8 ) # Ruban de 8 led multicolores connecté à la broche n°2
 
 eteinte = (0, 0, 0)
-couleurs = [(113,  15,   0),
-            ( 81,  47,   0),
-            ( 47,  81,   0),
-            ( 15, 113,   0),
-            (  0, 113,  15),
-            (  0,  81,  47),
-            (  0,  47,  81),
-            (  0,  15, 113)]
+couleurs = [(112,  16,   0),
+            ( 80,  48,   0),
+            ( 48,  80,   0),
+            ( 16, 112,   0),
+            (  0, 112,  16),
+            (  0,  80,  48),
+            (  0,  48,  80),
+            (  0,  16, 112)]
 
 for i in range(8):
     led[i] = couleurs[i]
 led.write()
 
-sleep(2)
+sleep(15)
 for i in range(8) : # éteindre
     led[i] = eteinte  
 led.write()
